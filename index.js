dd6f347c7ae080ee8fa8195af98e976ebed608c3
Shihab AL-Mekhlafi <shihabalmekhlafy@gmail.com>
2020-07-18 15:17:22 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Code your solutions in this file
+// Code your solutions in this file// Code your solutions in this file
+function writeCards(nameArray, eventName) {
+    let thankMessage = [];
+    for (let i = 0; i < nameArray.length; i++) {
+        thankMessage.push(`Thank you, ${nameArray[i]}, for the wonderful ${eventName} gift!`);
+          }
+
+    return thankMessage;
+}
+let test=[];
+
+test=writeCards(["Ada", "Brendan", "Ali"], "birthday");
+
+function countDown(number) {
+  let i =number;
+  while (i >= 0) {
+    console.log(i);
+    i--; 
+  }
+}
+ countDown(10);
