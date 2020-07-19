a9f8d96f03fa57bb5f43ca39130d7455b432750a
Sondos Ahmed <sondosalshami@gmail.com>
2020-07-19 09:29:30 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Code your solutions in this file
+
+function writeCards(arr, event){
+  for( let i=0; i<arr.length(); i++)
+   return "Thank you, "+arr[i]+" , for the wonderful "+event+" gift!";
+}
