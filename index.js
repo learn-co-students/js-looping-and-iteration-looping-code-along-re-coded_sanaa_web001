0114d81d16932c3ee3172b6e7730f11180b99648
Sondos Ahmed <sondosalshami@gmail.com>
2020-07-19 11:15:12 +0000

    Done.

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
 // Code your solutions in this file
 
-function writeCards(arr, event){
-  for( let i=0; i<arr.length(); i++)
-   return "Thank you, "+arr[i]+" , for the wonderful "+event+" gift!";
+const arr = ["Ada", "Brendan",  "Ali"];
+const event = "birthday";
+let s=[];
+function writeCards(arr, event) {
+    for (let i = 0; i < arr.length; i++)
+        s[i]=`Thank you, ${arr[i]}, for the wonderful ${event} gift!`;
+        return s;
 }
+writeCards(arr, event);
+
+
+function countDown(i){
+  while(i>=0){
+    console.log(i);
+    i--;
+  }
+}
+countDown(10);
