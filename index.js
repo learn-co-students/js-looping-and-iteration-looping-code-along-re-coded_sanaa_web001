da2ebab3210efa680cc81139d2a300adf811c72b
Nuha Al-Sameai <noha.pepeep1@gmail.com>
2020-07-20 02:11:47 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Code your solutions in this file
+
+
+let n="surprise";
+let arr=["Ada", "Brendan", "Ali"];
+function writeCards(array,n) {
+  
+  for (let i = 0; i < array.length; i++) {
+    array[i]='Thank you, '+array[i]+', for the wonderful '+n+' gift!';
+    console.log(array[i]);
+    
+  }
+ return array;
+}
+
+function countDown(n){
+  var i=n;
+  while(i>=0){
+    console.log(i);
+    i--;
+   
+  }
+}
+writeCards(arr,n);
+countDown(10);
+console.log(arr[0]);
+console.log(arr[1]);
+console.log(arr[2]);
+
