ac107201802a0ffabfb5db6e89f7e7c0ab53db9d
Osamah Gharad <osamah.gharred@gmail.com>
2020-07-28 22:08:41 +0000

    finished

--- index.js
+++ index.js
@@ -9,22 +9,22 @@ function countDown(n) {
 countDown(10);
 
 
-// function writeCards(cards ,birthday) {
-//     for (let i=0;i<cards.length;i++){
-//
-//       console.log(`Thank you ,${cards[i]} for the wonderful ${birthday} gift! `);
-//     }
-//     return cards;
-//   }
-//
-// let cards =["Lisa", "Kaitlin", "Jan"];
-//   writeCards(cards, "surprise");
-function writeCards(names, giftName) {
-  let output =[];
-    for (let i = 0; i < names.length; i++) {
-      output.push("Thank you, " + names[i] + ", for the wonderful " + giftName + " gift!");
-  }
-  return output;
-}
+function writeCards(cards ,birthday) {
+    for (let i=0;i<cards.length;i++){
 
-console.log(writeCards(["Lisa", "Kaitlin", "Jan"],"surprise"));
+      console.log(`Thank you ,${cards[i]} for the wonderful ${birthday} gift! `);
+    }
+    return cards;
+  }
+
+let cards =["Lisa", "Kaitlin", "Jan"];
+  writeCards(cards, "surprise");
+// function writeCards(names, giftName) {
+//   let output =[];
+//     for (let i = 0; i < names.length; i++) {
+//       output.push("Thank you, " + names[i] + ", for the wonderful " + giftName + " gift!");
+//   }
+//   return output;
+// }
+//
+// console.log(writeCards(["Lisa", "Kaitlin", "Jan"],"surprise"));
