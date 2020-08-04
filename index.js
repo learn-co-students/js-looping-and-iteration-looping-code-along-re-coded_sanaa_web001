e153dbf003c8e8ada0ca772f495bf54d29a7a2f8
Tahany Alswoidy <tahanyalswoidy@gmail.com>
2020-08-04 17:09:31 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+function writeCards(names,eventName){
+  const messages=[];
+  for(let i = 0; i<names.length; i++){
+    messages.push("Thank you, "+names[i]+", for the wonderful " +eventName+" gift!");
+  }
+  return messages;
+}
+function countDown(number){
+  for(;number>=0;number--){
+    console.log(number);
+  }  
+}
+console.log(writeCards(["Lisa", "Kaitlin", "Jan"], "surprise"));
+countDown(10);
