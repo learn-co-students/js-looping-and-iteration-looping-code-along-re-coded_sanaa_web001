b9320bdc9127a887d4521b52ebf6ab8ff3ed3621
Hajar Helal <hajerhelal44@gmail.com>
2020-08-06 00:21:24 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Code your solutions in this file
+const arr= ["Lisa", "Kaitlin", "Jan"];
+const event=["surprise"];
+
+function writeCards(arr,event) {
+  for (let i = 0; i < arr.length; i++){
+    console.log(`Thank you ${arr[i]} for the wonderful ${event} gift!`);}
+return arr,event;
+}
+
+writeCards(arr,event);
