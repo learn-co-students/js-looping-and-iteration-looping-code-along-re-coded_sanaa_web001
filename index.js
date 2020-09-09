03549a5743602b46a76b61440d92d5da11af1a7c
Hajar Helal <hajerhelal44@gmail.com>
2020-09-09 10:57:17 +0000

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,35 @@
 // Code your solutions in this file
-const arr= ["Lisa", "Kaitlin", "Jan"];
-const event=["surprise"];
+
+//const arr= ["Lisa", "Kaitlin", "Jan"];
+//const event=["surprise"];
 
 function writeCards(arr,event) {
+  let thankYouCards=[];
   for (let i = 0; i < arr.length; i++){
-    console.log(`Thank you ${arr[i]} for the wonderful ${event} gift!`);}
-return arr,event;
+    console.log(`Thank you ,${arr[i]} for the wonderful ${event} gift!`);
+    thankYouCards.push(`Thank you, ${arr[i]}, for the wonderful ${event} gift!`)
+  }
+return thankYouCards;
 }
 
-writeCards(arr,event);
+function countDown(number){
+
+  while( number>0){
+      console.log(number)
+    number--;
+
+}
+  console.log(number);
+}
+//writeCards(arr,event);
+
+
+
+// function wrapGifts(["Ada", "Brendan", "Ali"], "birthday"); {
+//   for (let i = 0; i < writeCards().length; i++) {
+//     console.log(`Thank you ${writeCards[i]} for the wonderful birthday gift!`);
+//   }
+//
+//   return  ;
+// }
+// wrapGifts(gifts);
